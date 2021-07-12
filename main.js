264bad3e42877bd3833171417a544491eb49500b
Johann <jakoloborodun@gmail.com>
2021-07-12 18:23:11 +0200

    Add a database .json file as storage for Heim data.

--- main.js
+++ main.js
@@ -1,78 +1,87 @@
-const list = document.querySelector('.list'),
-      items = document.querySelectorAll('.blocks_item')
-      listItems = document.querySelectorAll('.list_item')
-function filter() {
-    list.addEventListener('click', event => {
-        const targetId = event.target.dataset.id
-        const target = event.target
-        if(target.classList.contains('list_item')) {
-            listItems.forEach(listItem => listItem.classList.remove('active'))
-            target.classList.add('active')
+const readJSON = () => {
+  return fetch('db.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("HTTP error " + response.status);
         }
-        
-        switch(targetId) {
-            case 'all': 
-            getItems('blocks_item') 
-            break
-            case 'Baden':  
-            getItems(targetId)
-            break
-            case 'Bayern':
-                getItems(targetId)    
-            break
-            case 'Berlin':
-                getItems(targetId)  
-            break
-            case 'Brandenburg':
-                getItems(targetId) 
-            break
-            case 'Bremen':
-                getItems(targetId) 
-            break
-            case 'Hamburg':
-                getItems(targetId) 
-            break
-            case 'Hessen':
-                getItems(targetId) 
-            break
-            case 'Mecklenburg':
-                getItems(targetId) 
-            break
-            case 'Niedersachsen':
-                getItems(targetId) 
-            break
-            case 'Nordrhein':
-                getItems(targetId)
-            break
-            case 'Rheinland':
-                getItems(targetId) 
-            break
-            case 'Saarland':
-                getItems(targetId) 
-            break
-            case 'Sachsen': 
-            getItems(targetId) 
-            break
-            case 'Anhalt':
-                getItems(targetId) 
-            break
-            case 'Schleswig':
-                getItems(targetId) 
-            break
-            case 'Thuringen':
-                getItems(targetId) 
-            break
-        }
-    })
-}
-filter()
+        return response.json();
+      })
+      .then(json => {
+        return json;
+      })
+      .catch(function(e) {
+        console.log(e);
+      })
+};
 
-function getItems(className) {
-    items.forEach(item => {
-        if (item.classList.contains(className)) {
-            item.style.display = 'block'
-        } else {
-            item.style.display = 'none'
-        }
-    })
-}
+const filter = () => {
+  document.querySelector('.list').addEventListener('click', event => {
+    const target = event.target,
+        listItems = document.querySelectorAll('.list_item');
+    let targetId = target.dataset.id;
+
+    if (target.classList.contains('list_item')) {
+      listItems.forEach(listItem => listItem.classList.remove('active'));
+      target.classList.add('active')
+    }
+
+    if (targetId === 'all') {
+      targetId = 'blocks_item'
+    }
+    getItems(targetId);
+  })
+};
+
+const getItems = (className) => {
+  const items = document.querySelectorAll('.blocks_item');
+
+  items.forEach(item => {
+    if (item.classList.contains(className)) {
+      item.style.display = 'block'
+    }
+    else {
+      item.style.display = 'none'
+    }
+  })
+};
+
+const buildMenu = (items) => {
+  const elem = document.getElementById('bundesList');
+
+  elem.innerHTML += `<li class="list_item active list-group-item" data-id="all">Все земли</li>`;
+  for (let i = 0; i < items.length; i++) {
+    let item = `<li class="list_item list-group-item" data-id="${ items[i].id }">${ items[i].name } ${ items[i].rusName }</li>`;
+
+    elem.innerHTML += item
+  }
+};
+
+const buildHeimList = (items) => {
+  const container = document.getElementById('heimContainer');
+
+  for (let i = 0; i < items.length; i++) {
+    let image = "img/" + items[i].bundesId + "/" + items[i].name + ".png";
+    let item = `<div class="blocks_item ${ items[i].bundesId }">
+                  <div class="item">
+                    <div class="card">
+                      <img src="${ image }" class="card-img-top" alt="${ items[i].name }">
+                      <div class="card-body">
+                        <h5 class="card-title">${ items[i].rusName }</h5>
+                        <p class="card-text">${ items[i].desc }</p>
+                        <a href="${ items[i].link }" class="btn btn-primary">Смотреть YouTube</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+    `;
+
+    container.innerHTML += item
+  }
+};
+
+readJSON().then(function(response) {
+  buildMenu(response.bundesland);
+  buildHeimList(response.heim);
+});
+
+filter();
